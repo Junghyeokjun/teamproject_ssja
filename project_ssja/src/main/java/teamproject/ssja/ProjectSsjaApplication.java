8214cd9626455ed79a9af385570f49ff5df03bd7
admin <hyeokjun1218@gmail.com>
2024-05-17 12:16:20 +0900

    기본 템플릿

---
+++ project_ssja/src/main/java/teamproject/ssja/ProjectSsjaApplication.java
@@ -0,0 +1,15 @@
+package teamproject.ssja;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(value = {"teamproject.ssja.mapper"})
+public class ProjectSsjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSsjaApplication.class, args);
+	}
+
+}
