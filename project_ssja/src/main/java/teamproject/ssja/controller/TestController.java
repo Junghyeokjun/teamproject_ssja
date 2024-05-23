accb1da16d9d7cefc2ecc904bf3834a4f934aedc
admin <hyeokjun1218@gmail.com>
2024-05-23 14:49:12 +0900

    결제 페이지 개선 -상품데이터를 기반으로 DB와 대조해 수량확인기능 추가

--- project_ssja/src/main/java/teamproject/ssja/controller/TestController.java
+++ project_ssja/src/main/java/teamproject/ssja/controller/TestController.java
@@ -1,7 +1,14 @@
 package teamproject.ssja.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import teamproject.ssja.dto.ProductDetailDto;
+import teamproject.ssja.dto.ProductDto;
 
 @Controller
 @RequestMapping("/test")
@@ -27,6 +34,20 @@ public class TestController {
 	@RequestMapping("/purchase")
 	public String purchase() {
 		return "purchase";
+	}
+	@RequestMapping("/test_purchase")
+	public ModelAndView testPurchase(ModelAndView mv) {
+		List<ProductDto> dtos=new ArrayList<>();
+		dtos.add(new ProductDto(1, 1, 11, 11000, 2, "222", 4, 2323, 222, "테스트", "침대"));
+		dtos.add(new ProductDto(2, 1, 12, 12000, 2, "222", 4, 2323, 222, "테스트", "수납"));
+		dtos.add(new ProductDto(3, 1, 21, 21000, 2, "222", 4, 2323, 222, "테스트", "침구"));
+		mv.addObject("products", dtos);
+		mv.setViewName("purchase");
+		return mv;
+	}
+	@RequestMapping("/purchase_complete")
+	public String purchaseComplete() {
+		return "purchase_complete";
 	}
 
 }
