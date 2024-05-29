8ddb2c63b00ca8fa41f141d7e3bb44b9522b5489
admin <hyeokjun1218@gmail.com>
2024-05-29 16:06:31 +0900

    엔코더 수정

--- project_ssja/src/main/java/teamproject/ssja/configure/security/PassworEncoder.java
+++ project_ssja/src/main/java/teamproject/ssja/configure/security/PassworEncoder.java
@@ -11,6 +11,6 @@ public class PassworEncoder {
     @Bean
     public PasswordEncoder passwordEncoder() {
         //return new CustomBcryptPasswordEncoder();
-    	return new CustomNoOpPasswordEncoder();
+    	return new BCryptPasswordEncoder();
     }
 }
