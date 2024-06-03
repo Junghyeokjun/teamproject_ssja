da407825ee22811a0e654891551d7e142ede61c2
WingKill <zpswlsldi@gmail.com>
2024-06-03 16:50:46 +0900

    댓글 작업중. 리스트 불러오기. 첫 화면은 성공. 이후 페이징 넘버 누를 시 해당 부분 처리.

--- project_ssja/src/main/java/teamproject/ssja/controller/ReplyController.java
+++ project_ssja/src/main/java/teamproject/ssja/controller/ReplyController.java
@@ -4,9 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -22,11 +26,38 @@ public class ReplyController {
 	@Autowired
 	private ReplyService replyService;
 
-	//@GetMapping("/list")
-	public ResponseEntity<Map<String, Object>> getReplyList(@ModelAttribute Criteria criteria){
+	@GetMapping("/list")
+	public ResponseEntity<Map<String, Object>> getReplyList(@ModelAttribute Criteria criteria, @RequestParam("bno") long bno){
 		Map<String, Object> responseData = new HashMap<>();
-		responseData.put("replys", replyService.showReplys(criteria.getBno()));
-		responseData.put("pageMaker", new PageVO(replyService.getTotal(criteria.getBno()), criteria));
-		return ResponseEntity.ok().body(responseData);
+		try {
+			criteria.setBno(bno);
+			responseData.put("replys", replyService.showReplys(criteria.getBno()));
+			log.info("replys : " + responseData.get("reply_view"));
+			responseData.put("pageMaker", new PageVO(replyService.getTotal(criteria.getBno()), criteria));
+			log.info("pageMaker : " + responseData.get("pageMaker"));
+			return ResponseEntity.ok().body(responseData);			
+		} catch (Exception e) {
+			e.printStackTrace();
+			responseData.put("error", "댓글 목록을 불러오는데 실패했습니다.");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseData);
+		} 		
+	}
+	
+	@GetMapping("/list/{pageNum}/{amount}")
+	public ResponseEntity<Map<String, Object>> getReplyList(@ModelAttribute Criteria criteria, @PathVariable("pageNum") int pageNum, @PathVariable("amount") int amount){
+		Map<String, Object> responseData = new HashMap<>();
+		try {			
+			criteria.setPageNum(pageNum);
+			criteria.setAmount(amount);
+			responseData.put("replys", replyService.showReplys(criteria.getBno()));
+			log.info("replys : " + responseData.get("reply_view"));
+			responseData.put("pageMaker", new PageVO(replyService.getTotal(criteria.getBno()), criteria));
+			log.info("pageMaker : " + responseData.get("pageMaker"));
+			return ResponseEntity.ok().body(responseData);			
+		} catch (Exception e) {
+			e.printStackTrace();
+			responseData.put("error", "댓글 목록을 불러오는데 실패했습니다.");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseData);
+		} 		
 	}
 }
