f8220568fb88e03028b667e78e93900f3ee26369
WingKill <zpswlsldi@gmail.com>
2024-06-10 17:50:53 +0900

    컨트롤러에 미리 권한 처리 만들어놓기. 내일 테스트 예정

--- project_ssja/src/main/java/teamproject/ssja/controller/VenderController.java
+++ project_ssja/src/main/java/teamproject/ssja/controller/VenderController.java
@@ -1,7 +1,9 @@
 package teamproject.ssja.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,49 +19,73 @@ import teamproject.ssja.service.Product.ProductCategoryService;
 @RequestMapping("/vender")
 public class VenderController {
 
-	//vender, product를 가져오는 service autowired해야 함
-	//상품 카테고리를 가져오는 서비스 
+	// vender, product를 가져오는 service autowired해야 함
+	// 상품 카테고리를 가져오는 서비스
 	@Autowired
 	ProductCategoryService productCategoryService;
-	
+
 	@GetMapping("")
 	public String venderHome() {
 		return "redirect:/vender/product/list";
 	}
-	
+
 	@GetMapping("/product/write")
 	public String writeProduct(Model model) {
 		log.info("writeProduct()..");
 		// 카테고리
 		model.addAttribute("pcMains", productCategoryService.getPCMain());
-		
+
 		return "/vender/vender_write_product";
 	}
-	
+
 	@GetMapping("/product/category")
-	
+
 	@PostMapping("/product/add")
 	public String addOne() {
 		log.info("addOne()..");
-		// 
-		
+		//
+
 		// 초기화면으로 돌아가기(해당 페이지는 초기 화면 역할을 하며, 이후 변경할 예정)
 		return "/vender/vender_insert_product";
 	}
-	
+
 	@GetMapping("/product/list")
 	public String ProductList() {
 		log.info("ProductList()..");
-		// 
-		
+		//
+
 		return "/vender/vender_product_list";
 	}
-	
+
 	@GetMapping("/question/list")
 	public String showProductList() {
 		log.info("showProductList()..");
-		
-		//		
+
+		//
 		return "/vender/vender_qna_list";
 	}
+
+//	권한 처리 부분 우선 주석처리함
+//	// 권한 처리 부분
+//	private String venderAuth(String mappingStr) {
+//		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//		if (authentication != null && authentication.isAuthenticated()) {
+//			Object principal = authentication.getPrincipal();
+//			if (principal instanceof CustomPrincipal) {
+//				// 사용자가 로그인한 경우
+//				CustomPrincipal userDetails = (CustomPrincipal) principal;
+//				
+//				if (!userDetails.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_VENDOR"))) {
+//					// 사용자가 판매자 권한을 가지고 있지 않은 경우
+//					log.info("권한이 없습니다.");
+//					return "redirect:/";
+//				}
+//			}
+//		} else {
+//			// 사용자가 로그인하지 않은 경우
+//			log.info("로그인이 되지 않았습니다.");
+//			return "redirect:/login";
+//		}
+//		return mappingStr;
+//	}
 }
