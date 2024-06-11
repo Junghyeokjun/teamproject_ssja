3266aca138ac3aa5ef72b516c344c3e3a72b6dfa
welcomeglory <godbu2021@naver.com>
2024-06-11 15:38:23 +0900

    관리자 페이지 컨트롤러 return 타입 변경

--- project_ssja/src/main/java/teamproject/ssja/controller/adminpage/AdminPageController.java
+++ project_ssja/src/main/java/teamproject/ssja/controller/adminpage/AdminPageController.java
@@ -3,12 +3,12 @@ package teamproject.ssja.controller.adminpage;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.Slf4j;
 import teamproject.ssja.dto.MembersSearchDto;
@@ -20,7 +20,7 @@ import teamproject.ssja.service.Admin.ProductListService;
 import teamproject.ssja.service.Admin.PurchaseListService;
 
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/adminPage")
 public class AdminPageController {
 
@@ -33,51 +33,97 @@ public class AdminPageController {
 	@Autowired
 	private PurchaseListService purchaseListService;
 	
-	@RequestMapping("/membersList")
-	public ModelAndView membersList(ModelAndView model, Criteria criteria) {
+	@GetMapping("/membersList")
+	public String membersList(Model model, Criteria criteria) {
 		log.info("membersList()..");
 
 		long Memberstotal = memberListService.getMemberListTotalCount();
-		model.addObject("memberpageMaker", new Page10VO(Memberstotal, criteria));
-		model.addObject("members",memberListService.getMemberListWithPaging(criteria));
-		model.setViewName("/adminPage/membersList");
-		return  model;
+		model.addAttribute("memberpageMaker", new Page10VO(Memberstotal, criteria));
+		model.addAttribute("members",memberListService.getMemberListWithPaging(criteria));
+		return  "/adminPage/membersList";
 	}
 		
 	@GetMapping("/membersSearchList")
-	public List<MembersSearchDto> membersSearchList(@RequestParam("type") String type, @RequestParam("keyword") String keyword,Model model) {
-		log.info("membersSearchList()..");
-		return memberListService.getMemberSearchList(type,keyword);
-	}	
-
+    public ResponseEntity<List<MembersSearchDto>> membersSearchList(@RequestParam("type") String type, @RequestParam("keyword") String keyword) {
+        log.info("membersSearchList()..");
+        List<MembersSearchDto> searchResults = memberListService.getMemberSearchList(type, keyword);
+        return ResponseEntity.ok(searchResults);
+    }
 	@RequestMapping("/productsList")
-	public ModelAndView productsList(ModelAndView model, Criteria criteria) {
+	public String productsList(Model model, Criteria criteria) {
 		log.info("productsList()..");
 
 		long Productstotal = productListService.getProductListTotalCount();
-		model.addObject("productpageMaker", new Page10VO(Productstotal, criteria));
-		model.addObject("products", productListService.getProductListWithPaging(criteria));
-		model.setViewName("/adminPage/productsList");
+		model.addAttribute("productpageMaker", new Page10VO(Productstotal, criteria));
+		model.addAttribute("products", productListService.getProductListWithPaging(criteria));
 		
-		return model;
+		return "/adminPage/productsList";
 	}
 	
 	@GetMapping("/productsSearchList")
-	public List<ProductsSearchDto> productsSearchList(@RequestParam("type") String type, @RequestParam("keyword") String keyword,Model model) {
+	public ResponseEntity<List<ProductsSearchDto>> productsSearchList(@RequestParam("type") String type, @RequestParam("keyword") String keyword) {
 		log.info("productsSearchList()..");
-		return productListService.getProductsSearchList(type,keyword);
+        List<ProductsSearchDto> searchResults = productListService.getProductsSearchList(type,keyword);
+        return ResponseEntity.ok(searchResults);
 	}	
 	
 	@RequestMapping("/purchasesList")
-	public ModelAndView purchasesList(ModelAndView model, Criteria criteria) {
+	public String purchasesList(Model model, Criteria criteria) {
 		log.info("purchasesList()..");
 
 		long Purchasestotal = purchaseListService.getPerchaseListTotalCount();
-		model.addObject("purchasepageMaker", new Page10VO(Purchasestotal, criteria));
-		model.addObject("purchases", purchaseListService.getPerchaseListWithPaging(criteria));
-		model.setViewName("/adminPage/purchasesList");
+		model.addAttribute("purchasepageMaker", new Page10VO(Purchasestotal, criteria));
+		model.addAttribute("purchases", purchaseListService.getPerchaseListWithPaging(criteria));
 		
-		return model;
-	}
+		return "/adminPage/purchasesList";
+	}	
+	
+	
+//	@RequestMapping("/membersList")
+//	public ModelAndView membersList(ModelAndView model, Criteria criteria) {
+//		log.info("membersList()..");
+//
+//		long Memberstotal = memberListService.getMemberListTotalCount();
+//		model.addObject("memberpageMaker", new Page10VO(Memberstotal, criteria));
+//		model.addObject("members",memberListService.getMemberListWithPaging(criteria));
+//		model.setViewName("/adminPage/membersList");
+//		return  model;
+//	}
+	
+//	@GetMapping("/membersSearchList")
+//	public List<MembersSearchDto> membersSearchList(@RequestParam("type") String type, @RequestParam("keyword") String keyword,Model model) {
+//		log.info("membersSearchList()..");
+//		return memberListService.getMemberSearchList(type,keyword);
+//	}	
+
+//	@RequestMapping("/productsList")
+//	public ModelAndView productsList(ModelAndView model, Criteria criteria) {
+//		log.info("productsList()..");
+//
+//		long Productstotal = productListService.getProductListTotalCount();
+//		model.addObject("productpageMaker", new Page10VO(Productstotal, criteria));
+//		model.addObject("products", productListService.getProductListWithPaging(criteria));
+//		model.setViewName("/adminPage/productsList");
+//		
+//		return model;
+//	}
+//	
+//	@GetMapping("/productsSearchList")
+//	public List<ProductsSearchDto> productsSearchList(@RequestParam("type") String type, @RequestParam("keyword") String keyword,Model model) {
+//		log.info("productsSearchList()..");
+//		return productListService.getProductsSearchList(type,keyword);
+//	}	
+//	
+//	@RequestMapping("/purchasesList")
+//	public ModelAndView purchasesList(ModelAndView model, Criteria criteria) {
+//		log.info("purchasesList()..");
+//
+//		long Purchasestotal = purchaseListService.getPerchaseListTotalCount();
+//		model.addObject("purchasepageMaker", new Page10VO(Purchasestotal, criteria));
+//		model.addObject("purchases", purchaseListService.getPerchaseListWithPaging(criteria));
+//		model.setViewName("/adminPage/purchasesList");
+//		
+//		return model;
+//	}
 	
 }
