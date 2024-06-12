04c3fb69d9f7443bd2c6664fc5b4105b28607722
cajunSub <junsyub@gmail.com>
2024-06-12 10:53:00 +0900

    6012푸쉬 관리자 crud공지사항 진행 중

--- project_ssja/src/main/java/teamproject/ssja/controller/adminpage/AdminPageController.java
+++ project_ssja/src/main/java/teamproject/ssja/controller/adminpage/AdminPageController.java
@@ -3,9 +3,11 @@ package teamproject.ssja.controller.adminpage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import teamproject.ssja.InfoProvider;
 import teamproject.ssja.page.Criteria;
 import teamproject.ssja.page.Page10VO;
 import teamproject.ssja.service.Admin.MemberListService;
@@ -44,4 +46,11 @@ public class AdminPageController {
 		return "/adminPage/productsList";
 	}
 
+	
+	@GetMapping("/noticeList")
+	public String adminNoticePage() {
+		
+		return "/adminPage/noticeMain";
+	}
+	
 }
