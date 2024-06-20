ca1d925bc6b4384b43ac62371403f30cdddcaff4
admin <hyeokjun1218@gmail.com>
2024-06-20 12:34:25 +0900

    -상대경로 매핑

--- project_ssja/src/main/java/teamproject/ssja/configure/WebConfig.java
+++ project_ssja/src/main/java/teamproject/ssja/configure/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
     	//현재 상대경로를 이용하여 매핑중이므로 사용중지 
-//        registry.addResourceHandler("/images/board_content/**")
-//                .addResourceLocations("classpath:src/main/resources/static/images/board_content/");
+        registry.addResourceHandler("/images/board_content/**")
+        	.addResourceLocations("file:/home/ubuntu/images/");
     }
 }
