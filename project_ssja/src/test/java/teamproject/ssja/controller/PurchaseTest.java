70a86208682a9d033a65571a093b8d62b3f34abb
admin <hyeokjun1218@gmail.com>
2024-06-26 14:07:10 +0900

    -purchaseControllerTest 보완

--- project_ssja/src/test/java/teamproject/ssja/controller/PurchaseTest.java
+++ project_ssja/src/test/java/teamproject/ssja/controller/PurchaseTest.java
@@ -3,9 +3,6 @@ package teamproject.ssja.controller;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +12,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -51,14 +48,38 @@ class PurchaseTest {
 
 	@Disabled
 	@Test
-	void testPurchase() throws Exception {
-        String responseJson = "{\"M_NO\":\"21\", \"PUR_TOT\":\"84431\", \"PUR_DC\":\"84431\", \"PUR_PAY\":\"0\", \"PUR_PAYMENT\":\"discount\", \"PUR_DVADDRESS\":\"부산 남구 진남로232번길 2 1층\", \"PUR_DV\":\"대한통운\", \"USE_POINT\":\"84431\", \"products\":[{\"product_no\":\"2689\", \"price\":\"84431\", \"quantity\":\"1\", \"discount\":\"84431\", \"pay\":\"0\", \"coupon\":\"0\"}]}";
+	void testPurchase() throws Exception {		
+		
+		MultiValueMap<String, String> data= new LinkedMultiValueMap<String, String>(); 
+		
+		data.add("M_NO", "21");
+		data.add("PUR_TOT", "84431");
+		data.add("PUR_DC", "84431");
+		data.add("PUR_PAY", "0");
+		data.add("PUR_PAYMENT", "discount");
+		data.add("PUR_DVADDRESS", "부산 남구 진남로232번길 2 1층");
+		data.add("PUR_DV", "대한통운");
+		data.add("USE_POINT", "84431");
+		data.add("products[0][product_no]", "2689");
+		data.add("products[0][price]", "84431");
+		data.add("products[0][quantity]", "1");
+		data.add("products[0][pay]", "0");
+		data.add("products[0][coupon]", "0");
+		
+//        String responseJson = "{\"M_NO\":\"21\", \"PUR_TOT\":\"84431\", \"PUR_DC\":\"84431\", \"PUR_PAY\":\"0\", \"PUR_PAYMENT\":\"discount\", \"PUR_DVADDRESS\":\"부산 남구 진남로232번길 2 1층\", \"PUR_DV\":\"대한통운\", \"USE_POINT\":\"84431\", \"products[0][product_no]\":\"2689\", \"products[0][price]\":\"84431\", \"products[0][quantity]\":\"1\", \"products[0][discount]\":\"84431\", \"products[0][pay]\":\"0\", \"products[0][coupon]\":\"0\"}";
 		mockMvc.perform(MockMvcRequestBuilders.post("/purchase/success")
-			   .param("data",responseJson)
+			   .params(data)
 			   .contentType(MediaType.APPLICATION_JSON)
 			   .with(csrf())
 			   .accept(MediaType.APPLICATION_JSON))
 			   .andExpect(MockMvcResultMatchers.status().isOk())
 			   .andDo(print());	}
 
+//	@Disabled
+	@Test
+	void testPurchaseCompletePage() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/purchase/complete")
+			   .param("price", "11111"))
+			   .andExpect(MockMvcResultMatchers.status().isOk())
+			   .andDo(print());	}
 }
