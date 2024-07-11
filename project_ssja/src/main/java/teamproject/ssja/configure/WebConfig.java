48dc5809cb0006d8c707a4e9eaa5f6d2ad76ef74
admin <hyeokjun1218@gmail.com>
2024-07-11 09:56:32 +0900

    이미지 경로 2차수정

--- project_ssja/src/main/java/teamproject/ssja/configure/WebConfig.java
+++ project_ssja/src/main/java/teamproject/ssja/configure/WebConfig.java
@@ -13,6 +13,8 @@ public class WebConfig implements WebMvcConfigurer {
     	//현재 상대경로를 이용하여 매핑중이므로 사용중지 
     	registry.addResourceHandler("/images/board_content/**")
     	.addResourceLocations("file:/home/ubuntu/images/");
+    	registry.addResourceHandler("/images/review/**")
+    	.addResourceLocations("file:/home/ubuntu/images/");
 //    	registry.addResourceHandler("/images/board_content/**")
 //      .addResourceLocations("file://desktop-rduhp84/board_content/");
     }
